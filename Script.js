d07f4370729be8943b8e355819cfc417157903cc
Diego Viña <diegovinatrillo@gmail.com>
2024-08-29 01:25:21 +0200

    Advancing in the understanding of the event listeners

--- Script.js
+++ Script.js
@@ -4,13 +4,13 @@ const carousel = container.querySelector(".carousel");
 const cItems = carousel.querySelectorAll(".c-item");
 
 let isMouseDown = false;
-let mouseCurreentPos = 0;
+let mouseCurrentPos = 0;
 let mouseLastPos = 0;
 let lastMoveTo = 0;
 let moveTo = 0;
 
 const createCarousel = () => {
-    const carouselProps = onresize();
+    const carouselProps = onResize();
     const length = cItems.length;
     const degrees = 360 / length;
     const gap = 20;
@@ -25,7 +25,7 @@ const createCarousel = () => {
         item.style.setProperty("--rotatey", degreesByItem);
         item.style.setProperty("--translatez", zTravel + "px");
     });
-}
+};
 
 //Function to make the animation smoother
 const linearInterpolation = (a, n, b) => {
@@ -34,7 +34,7 @@ const linearInterpolation = (a, n, b) => {
 
 const distanceZ = (widthElement, length, gap) => {
     return widthElement / 2 / Math.tan(Math.PI / length) + gap;
-}
+};
 
 /*OK, here we calculate the height using trigonometry and angles.
 I'm not really a math guy so I consulted GPT. You could remove the division by 2 since you
@@ -44,3 +44,87 @@ const calculateHeight = (z) => {
     const height = t * 2 * z;
     return height;
 };
+
+const calculateFov = (carouselProps) => {
+    //Retrieve the perspective value
+    const perspective = window
+    .getComputedStyle(carouselContainer)
+    .perspective.split("px")[0];
+
+    //Calculating the diagonal lenght of the carousel
+    const length = 
+    Math.sqrt(carouselProps.w * carouselProps.w)+
+    Math.sqrt(carouselProps.h * carouselProps.h);
+
+    const fov = 2 * Math.atan(length/ (2 * perspective)) * (180 / Math.PI);
+    return fov;
+};
+
+//Obtains position in X axis and analizes if it should rotate left or right
+const getPosX = (x) => {
+    mouseCurrentPos = x
+
+    moveTo = mouseCurrentPos < mouseLastPos ? moveTo -2 : moveTo +2;
+
+    mouseLastPos = mouseCurrentPos;
+};
+
+const update = () => {
+    lastMoveTo = linearInterpolation(moveTo, lastMoveTo, 0.05);
+    carousel.style.setProperty("--rotatey", lastMoveTo, + "deg");
+
+    requestAnimationFrame(update);//Syncs the browser's refresh rate with update 
+};
+
+//Allows oither poarts of the code to dynamically adjust the carousel's size and layout
+const onResize = () => {
+    const boundingCarousel = carouselContainer.getBoundingClientRect()//Returns a DOMRect object with sizing (w, h, top, right...);
+
+    const carouselProps = {
+        w: boundingCarousel.width,
+        h: boundingCarousel.height,
+    };
+
+    return carouselProps
+};
+
+const initEvents = () => {
+
+    //First the mouse events
+
+    carousel.addEventListener("mouseDown", () => {
+        isMouseDown = true;
+        carousel.style.cursor = "grabbing";
+    });
+
+    carousel.addEventListener("mouseUp", () => {
+        isMouseDown = false;
+        carousel.style.cursor = "grab";
+    });
+
+    container.addEventListener("mouseLeave", () => isMouseDown = false);
+
+    carousel.addEventListener("mouseMove", (e) => isMouseDown && getPosX(e.clientX));
+
+    //Now the touchscreen events
+
+    carousel.addEventListener("touchStart", () => {
+        isMouseDown = true;
+        carousel.style.cursor = "grabbing"
+    });
+
+    carousel.addEventListener("touchEnd", () => {
+        isMouseDown = false;
+        carousel.style.cursor = "grab";
+    });
+
+    carousel.addEventListener("touchMove", (e) => isMouseDown && getPosX(e.touches[0].clientX));
+
+    window.addEventListener("resize", createCarousel);
+
+    update();
+
+    createCarousel();
+}
+
+initEvents()
